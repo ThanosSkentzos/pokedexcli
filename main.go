c8a710f773fbf98aab63d83a1e43bc4b2104594c
ThanosSkentzos <kalligerist@gmail.com>
2025-01-13 15:36:20 +0100

    basic io

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main(){
-	fmt.Println("Hello, World!")
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		s.Scan()
+		text := s.Text()
+		words := strings.Fields(strings.ToLower(text))
+		fmt.Printf("Your command was: %v\n",words[0])
+
+	}
 }
 
